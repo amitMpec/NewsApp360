d77d45e231fe6a159e8707ed033a388883590664
amitMpec <74000778+amitMpec@users.noreply.github.com>
2022-02-19 12:36:07 +0530

    Update NewsInterface.java

--- app/src/main/java/com/example/news360/data/NewsInterface.java
+++ app/src/main/java/com/example/news360/data/NewsInterface.java
@@ -11,7 +11,8 @@ public interface NewsInterface {
     //https://newsapi.org/v2/top-headlines?country=in&apiKey=API_KEY
     //https://newsapi.org/v2/everything?q=apple&from=2021-09-23&to=2021-09-23&sortBy=popularity&apiKey=API_KEY
 
-     String API_KEY = "ade554c08fdb47e7bd7938178f198648";
+    // Add your own api key
+     String API_KEY = "5c206f15-d484-xxxxx-xxxx-xxxxxxxx";
     @GET(value = "v2/top-headlines?apiKey=" + API_KEY)
     Call<News> getHeadlines(@Query(value = "country") String country);
 
